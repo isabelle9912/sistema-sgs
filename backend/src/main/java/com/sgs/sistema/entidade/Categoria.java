799ef2088b3196c9a8e89fdd2efc18fa3742482f
isabelle9912 <isabellesaahirah28@gmail.com>
2026-08-07 17:25:51 -0300

    Adiciona a entidade Categoria com validações e mapeamento JPA

---
+++ backend/src/main/java/com/sgs/sistema/entidade/Categoria.java
@@ -0,0 +1,23 @@
+package com.sgs.sistema.entidade;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categoria")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome da categoria é obrigatório")
+    @Column(nullable = false, length = 100)
+    private String nome;
+}
