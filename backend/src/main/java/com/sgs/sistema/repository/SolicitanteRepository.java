5a195e67ea7a5aa106a36bb4b8e0104b383a878a
isabelle9912 <isabellesaahirah28@gmail.com>
2026-08-07 18:14:52 -0300

    Adiciona o repositório SolicitanteRepository para gerenciamento de solicitantes

---
+++ backend/src/main/java/com/sgs/sistema/repository/SolicitanteRepository.java
@@ -0,0 +1,7 @@
+package com.sgs.sistema.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sgs.sistema.entidade.Solicitante;
+
+public interface SolicitanteRepository extends JpaRepository<Solicitante, Long> {}
