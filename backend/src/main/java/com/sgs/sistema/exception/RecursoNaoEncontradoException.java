eb5913244c45fca9e126123f37bd0daacbbe6c18
isabelle9912 <isabellesaahirah28@gmail.com>
2026-08-07 17:29:55 -0300

    Adiciona a exceção personalizada RecursoNaoEncontradoException para tratamento de recursos não encontrados

---
+++ backend/src/main/java/com/sgs/sistema/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.sgs.sistema.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+    
+}
